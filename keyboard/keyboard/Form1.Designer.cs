b00bc3c6da036210792bc2e73e237896ef301a0a
mariaK228 <mkorol2023@gmail.com>
2018-08-28 11:31:12 +0300

    Stupid keyboard prorgam

---
+++ keyboard/keyboard/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace keyboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.showAnyLetter = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.yourText = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // showAnyLetter
+            // 
+            this.showAnyLetter.BackColor = System.Drawing.Color.LightGreen;
+            this.showAnyLetter.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.showAnyLetter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.showAnyLetter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.showAnyLetter.Location = new System.Drawing.Point(61, 63);
+            this.showAnyLetter.Name = "showAnyLetter";
+            this.showAnyLetter.ReadOnly = true;
+            this.showAnyLetter.Size = new System.Drawing.Size(537, 31);
+            this.showAnyLetter.TabIndex = 0;
+            this.showAnyLetter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(243, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // yourText
+            // 
+            this.yourText.BackColor = System.Drawing.Color.White;
+            this.yourText.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yourText.Location = new System.Drawing.Point(57, 188);
+            this.yourText.Name = "yourText";
+            this.yourText.Size = new System.Drawing.Size(537, 38);
+            this.yourText.TabIndex = 5;
+            this.yourText.TextChanged += new System.EventHandler(this.yourText_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.White;
+            this.progressBar1.Location = new System.Drawing.Point(61, 330);
+            this.progressBar1.Maximum = 3;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(537, 56);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(216, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите символ: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(673, 428);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.yourText);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showAnyLetter);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox showAnyLetter;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox yourText;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ keyboard/keyboard/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keyboard
+{
+    public partial class Form1 : Form
+    {
+        char[] rusletters = new char[32];
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int count = 0;
+            for (char i = 'а'; i <= 'я'; i++)
+            {
+                rusletters[count] = i;
+                count++;
+            }
+        }
+
+        Random rnd = new Random();
+
+        private char getRandLetter()
+        {           
+            char letter = rusletters[rnd.Next(0, rusletters.Length)];
+
+            return letter;
+        }
+        int lastTime;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            timer1.Interval = 5000;
+            progressBar1.Value = 0;
+            lastTime = 10000;
+            yourText.Focus(); 
+            startFunction();    
+        }
+       private void startFunction()
+        {
+            if (timer1.Interval > 1000)
+            {
+                timer1.Interval = lastTime - 1000;
+                lastTime = timer1.Interval;
+                timer1.Enabled = true;
+                timer1.Stop();
+                timer1.Start();
+                yourText.Enabled = true;
+                showAnyLetter.Text = getRandLetter().ToString();
+            }
+            else
+            {
+                yourText.Enabled = false;
+                timer1.Enabled = false;
+                MessageBox.Show("Капец ты мощный. Роботам не победить");
+            }
+            
+            
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("Ты проиграл");
+            
+            yourText.Enabled = false;
+        }
+        private void checkLetter ()
+        {
+
+            string savetext = yourText.Text;
+            char lastletter = savetext[yourText.TextLength-1];
+            string nowrow = showAnyLetter.Text;
+            char nowLetter = nowrow[showAnyLetter.TextLength-1];
+
+
+            if (lastletter != nowLetter && timer1.Enabled == true && progressBar1.Value < 3)
+            { 
+                progressBar1.Value++;
+                if (timer1.Interval > 0)
+                    startFunction();
+               
+            }
+            else if (lastletter != nowLetter && timer1.Enabled == true && progressBar1.Value == 3)
+            {
+                 timer1.Enabled = false;
+                MessageBox.Show("Ты проиграл");
+                yourText.Enabled = false;
+                
+            }
+            else 
+            {
+                if (timer1.Interval > 0)
+                startFunction();
+            }
+        }
+
+        private void yourText_TextChanged(object sender, EventArgs e)
+        {            
+                checkLetter();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            showAnyLetter.Enabled = false;           
+        }       
+    }
+}
