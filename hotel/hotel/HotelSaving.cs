ffc7c9aebc681bf03e8dbc197b8a98b86f6cb6cb
DimKaHUI <wet75787@tqosi.com>
2018-10-27 23:05:39 +0300

    Braces pos fixed

--- hotel/hotel/HotelSaving.cs
+++ hotel/hotel/HotelSaving.cs
@@ -8,25 +8,26 @@ namespace hotel
 {
     class HotelSaving
     {
-       List <GuestsStruct> guest = new List <GuestsStruct>();
+        List<GuestsStruct> guest = new List<GuestsStruct>();
 
-       public GuestsStruct AddGuest (string surname, string room, string dish)
-       {
-           GuestsStruct newguest = new GuestsStruct(surname, room, dish);
-           guest.Add(newguest);
+        public GuestsStruct AddGuest(string surname, string room, string dish)
+        {
+            GuestsStruct newguest = new GuestsStruct(surname, room, dish);
+            guest.Add(newguest);
 
-           return newguest;
-       }
+            return newguest;
+        }
 
 
         public GuestsStruct[] GetPersons()
         {
             return guest.ToArray();
         }
+
         public void WriteGuests(Stream stream)
-       {
-         
-           
-       }
+        {
+
+
+        }
     }
 }
