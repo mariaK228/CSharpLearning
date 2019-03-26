6a58a2a6db559be904f0bee8ebc11e27b8c83749
mariaK228 <mkorol2023@gmail.com>
2019-03-26 19:50:03 +0300

    1.0

---
+++ PishitePisma/PishitePisma/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PishitePisma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputInputbox = new System.Windows.Forms.RichTextBox();
+            this.MessageDisplay = new System.Windows.Forms.RichTextBox();
+            this.SendMessage = new System.Windows.Forms.Button();
+            this.UserIP = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InputInputbox
+            // 
+            this.InputInputbox.Location = new System.Drawing.Point(32, 379);
+            this.InputInputbox.Name = "InputInputbox";
+            this.InputInputbox.Size = new System.Drawing.Size(291, 49);
+            this.InputInputbox.TabIndex = 0;
+            this.InputInputbox.Text = "";
+            // 
+            // MessageDisplay
+            // 
+            this.MessageDisplay.Location = new System.Drawing.Point(32, 42);
+            this.MessageDisplay.Name = "MessageDisplay";
+            this.MessageDisplay.Size = new System.Drawing.Size(291, 303);
+            this.MessageDisplay.TabIndex = 1;
+            this.MessageDisplay.Text = "";
+            // 
+            // SendMessage
+            // 
+            this.SendMessage.Location = new System.Drawing.Point(135, 434);
+            this.SendMessage.Name = "SendMessage";
+            this.SendMessage.Size = new System.Drawing.Size(75, 23);
+            this.SendMessage.TabIndex = 2;
+            this.SendMessage.Text = "Отправить";
+            this.SendMessage.UseVisualStyleBackColor = true;
+            this.SendMessage.Click += new System.EventHandler(this.SendMessage_Click);
+            // 
+            // UserIP
+            // 
+            this.UserIP.Location = new System.Drawing.Point(32, 16);
+            this.UserIP.Name = "UserIP";
+            this.UserIP.Size = new System.Drawing.Size(291, 20);
+            this.UserIP.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 461);
+            this.Controls.Add(this.UserIP);
+            this.Controls.Add(this.SendMessage);
+            this.Controls.Add(this.MessageDisplay);
+            this.Controls.Add(this.InputInputbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox InputInputbox;
+        private System.Windows.Forms.RichTextBox MessageDisplay;
+        private System.Windows.Forms.Button SendMessage;
+        private System.Windows.Forms.TextBox UserIP;
+    }
+}
+
---
+++ PishitePisma/PishitePisma/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PishitePisma
+{
+    public partial class Form1 : Form
+    {
+        public const int Port = 25565;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessage_Click(object sender, EventArgs e)
+        {
+            string Text = InputInputbox.Text;
+            byte[] data = Encoding.Unicode.GetBytes(Text);
+
+            SendMessage(data, IPAddress.Parse(UserIP.Text));
+        }
+
+        void Listener()
+        {
+            UdpClient udp = new UdpClient(Port);
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint IPEP = new IPEndPoint(IPAddress.Any, Port);
+                    byte[] data = udp.Receive(ref IPEP);
+                    IPAddress sender = IPEP.Address;
+                    MessageBox.Show("Message from " + sender + "\n" + Encoding.Unicode.GetString(data));
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+
+            }
+        }
+
+        void SendMessage(byte[] data, IPAddress destination)
+        {
+            UdpClient udp = new UdpClient(Port);
+            IPEndPoint IPEP = new IPEndPoint(destination, Port);
+            udp.Send(data, data.Length, IPEP);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread listenerThread = new Thread(Listener);
+            listenerThread.Start();
+
+        }
+    }
+}
