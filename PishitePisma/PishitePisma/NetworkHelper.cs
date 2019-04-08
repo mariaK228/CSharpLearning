d7fa9aa636dfe7c952c39c205d4e3c91df0d1c51
mariaK228 <42766063+mariaK228@users.noreply.github.com>
2019-04-08 20:45:26 +0300

    NetworkHelper

---
+++ PishitePisma/PishitePisma/NetworkHelper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PishitePisma
+{
+    public delegate void NetCallback(byte[] pkg, IPAddress sender);
+    class NetworkHelper
+    {
+        public const int Port = 25565;
+        void SendPackage(byte[] pkg, IPAddress destination)
+        {
+            UdpClient udp = new UdpClient();
+            udp.ExclusiveAddressUse = false;
+            IPEndPoint endPoint = new IPEndPoint(destination, Port);
+            udp.Send(pkg, pkg.Length);
+        }
+
+        void SetSyncReceiveCallback(byte[] callback)
+        {
+            UdpClient udp = new UdpClient();
+
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, Port);
+                callback = udp.Receive(ref endPoint);
+            }
+
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error:" + ex);
+            }
+        }
+    }
+}
